7d1769d67442d3f940a5ca7b4dfbd67b21cc6e7e
visal sandeep adikari <122593452+visaladi@users.noreply.github.com>
2024-05-11 10:56:56 +0530

    Add files via upload

---
+++ Client/src/client/Main_Client.java
@@ -0,0 +1,376 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import data.DataFileServer;
+import data.DataReader;
+import io.socket.client.Ack;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import java.awt.Component;
+import java.io.File;
+import java.net.URISyntaxException;
+import javax.swing.JFileChooser;
+import javax.swing.JTable;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import org.json.JSONException;
+import org.json.JSONObject;
+import swing.CellEditor;
+import swing.CellEditorFile;
+
+
+
+/**
+ *
+ * @author Visal
+ */
+public class Main_Client extends javax.swing.JFrame {
+
+
+    public Main_Client() {
+        initComponents();
+        model = (DefaultTableModel) table.getModel();
+        modelFile = (DefaultTableModel) tableFile.getModel();
+        table.getColumnModel().getColumn(4).setCellRenderer(new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int i, int i1) {
+                Component com = super.getTableCellRendererComponent(jtable, o, bln, bln1, i, i1);
+                Object data = jtable.getValueAt(i, 0);
+                if (data instanceof DataReader) {
+                    DataReader reader = (DataReader) data;
+                    Component c = reader.getStatus();
+                    c.setBackground(com.getBackground());
+                    return c;
+                } else {
+                    return com;
+                }
+            }
+        });
+        table.getColumnModel().getColumn(4).setCellEditor(new CellEditor());
+        tableFile.getColumnModel().getColumn(4).setCellRenderer(new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int i, int i1) {
+                Component com = super.getTableCellRendererComponent(jtable, o, bln, bln1, i, i1);
+                DataFileServer data = (DataFileServer) jtable.getValueAt(i, 0);
+                Component c = data.getItem();
+                c.setBackground(com.getBackground());
+                return c;
+            }
+        });
+        tableFile.getColumnModel().getColumn(4).setCellEditor(new CellEditorFile());
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table = new javax.swing.JTable();
+        txtName = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        cmdConnect = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tableFile = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        table.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Data", "No", "File Name", "Size", "Status"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, true
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(table);
+        if (table.getColumnModel().getColumnCount() > 0) {
+            table.getColumnModel().getColumn(0).setMinWidth(0);
+            table.getColumnModel().getColumn(0).setPreferredWidth(0);
+            table.getColumnModel().getColumn(0).setMaxWidth(0);
+            table.getColumnModel().getColumn(1).setPreferredWidth(50);
+        }
+
+        jLabel1.setText("Name");
+
+        cmdConnect.setText("Connect");
+        cmdConnect.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdConnectActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("File");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmdConnect)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cmdConnect))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 299, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addContainerGap())
+        );
+
+        jTabbedPane1.addTab("Client", jPanel1);
+
+        tableFile.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Data", "No", "FIle Name", "Size", "Status"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, true
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(tableFile);
+        if (tableFile.getColumnModel().getColumnCount() > 0) {
+            tableFile.getColumnModel().getColumn(0).setMinWidth(0);
+            tableFile.getColumnModel().getColumn(0).setPreferredWidth(0);
+            tableFile.getColumnModel().getColumn(0).setMaxWidth(0);
+            tableFile.getColumnModel().getColumn(1).setPreferredWidth(50);
+        }
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 733, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 379, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("File On Server", jPanel2);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jTabbedPane1)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jTabbedPane1)
+                .addGap(0, 0, 0))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+    private final int DEFAULT_PORT = 4040;
+    private final DefaultTableModel model;
+    private final DefaultTableModel modelFile;
+    private Socket client;
+    private String IP = "localhost";
+    private void cmdConnectActionPerformed(java.awt.event.ActionEvent evt) {
+        if (client == null) {
+            try {
+                client = IO.socket("http://" + IP + ":" + DEFAULT_PORT);
+                //  Add event to client
+                client.on("exit_app", new Emitter.Listener() {
+                    @Override
+                    public void call(Object... os) {
+                        System.exit(0);
+                    }
+                });
+                client.on("new_file", new Emitter.Listener() {
+                    @Override
+                    public void call(Object... os) {
+
+                        try {
+                            addFile(new DataFileServer((JSONObject) os[0], table, client));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                client.open();
+                String userName = txtName.getText().trim();
+                client.emit("set_user", userName);
+                client.emit("request", "list_file", new Ack() {
+                    @Override
+                    public void call(Object... os) {
+                        try {
+                            for (Object o : os) {
+                                addFile(new DataFileServer((JSONObject) o, table, client));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            } catch (URISyntaxException e) {
+                System.err.println(e);
+            }
+        } else {
+            client.emit("set_user", txtName.getText().trim());
+        }
+    }//GEN-LAST:event_cmdConnectActionPerformed
+
+//    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+//        JFileChooser ch = new JFileChooser();
+//        ch.setMultiSelectionEnabled(true);
+//        int opt = ch.showOpenDialog(this);
+//        if (opt == JFileChooser.APPROVE_OPTION) {
+//            File[] files = ch.getSelectedFiles();
+//            for (File file : files) {
+//                try {
+//                    DataReader reader = new DataReader(file, table);
+//                    model.addRow(reader.toRowTable(table.getRowCount() + 1));
+//                    reader.startSend(client);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+    //GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        JFileChooser ch = new JFileChooser();
+        ch.setMultiSelectionEnabled(true);
+        // Add a file filter to accept only PDF files
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF Files", "pdf");
+        ch.setFileFilter(filter);
+        int opt = ch.showOpenDialog(this);
+        if (opt == JFileChooser.APPROVE_OPTION) {
+            File[] files = ch.getSelectedFiles();
+            for (File file : files) {
+
+                if (file.getName().toLowerCase().endsWith(".pdf")) {
+                    try {
+                        DataReader reader = new DataReader(file, table);
+                        model.addRow(reader.toRowTable(table.getRowCount() + 1));
+                        reader.startSend(client);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    // Display an error message or handle invalid file selection
+                    System.out.println("Only PDF files are allowed for upload.");
+                }
+            }
+        }
+    }
+
+
+
+
+    private void addFile(DataFileServer data) {
+        modelFile.addRow(data.toTableRow(tableFile.getRowCount() + 1));
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Main_Client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Main_Client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Main_Client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main_Client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main_Client().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdConnect;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JTable table;
+    private javax.swing.JTable tableFile;
+    private javax.swing.JTextField txtName;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ Client/src/data/DataFileServer.java
@@ -0,0 +1,170 @@
+
+package data;
+
+import io.socket.client.Ack;
+import io.socket.client.Socket;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.JTable;
+import org.json.JSONException;
+import org.json.JSONObject;
+import swing.PanelStatus_Item;
+
+/**
+ *
+ * @author Visal
+ */
+public class DataFileServer {
+
+    public PanelStatus_Item getItem() {
+        return item;
+    }
+
+    public void setItem(PanelStatus_Item item) {
+        this.item = item;
+    }
+
+    public int getFileID() {
+        return fileID;
+    }
+
+    public void setFileID(int fileID) {
+        this.fileID = fileID;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public File getOutPutPath() {
+        return outPutPath;
+    }
+
+    public void setOutPutPath(File outPutPath) {
+        this.outPutPath = outPutPath;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public DataFileServer(int fileID, String fileName, String fileSize, File outPutPath, boolean status) {
+        this.fileID = fileID;
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.outPutPath = outPutPath;
+        this.status = status;
+    }
+
+    public DataFileServer(JSONObject json, JTable table, Socket socket) throws JSONException {
+        fileID = json.getInt("fileID");
+        fileName = json.getString("fileName");
+        fileSize = json.getString("fileSize");
+        fileSizeLength = json.getLong("fileSizeLength");
+        item = new PanelStatus_Item();
+        this.table = table;
+        this.socket = socket;
+        item.addEventSave(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                JFileChooser ch = new JFileChooser();
+                ch.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int opt = ch.showSaveDialog(null);
+                if (opt == JFileChooser.APPROVE_OPTION) {
+                    outPutPath = new File(ch.getSelectedFile().getAbsolutePath() + "/" + fileName);
+                    item.startFile();
+                    try {
+                        saveFile();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        item.addEvent(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                //    Pause and resume
+                if (!item.isPause() && pause) {
+                    pause = false;
+                    try {
+                        saveFile();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private int fileID;
+    private String fileName;
+    private String fileSize;
+    private long fileSizeLength;
+    private File outPutPath;
+    private boolean status;
+    private PanelStatus_Item item;
+    private JTable table;
+    private DataWriter writer;
+    private Socket socket;
+    private boolean pause;
+
+    private void saveFile() throws IOException, JSONException {
+
+        if (writer == null) {
+            writer = new DataWriter(outPutPath, fileSizeLength);
+        }
+
+        JSONObject data = new JSONObject();
+        data.put("fileID", fileID);
+        data.put("length", writer.getFileLength());
+        socket.emit("request_file", data, new Ack() {
+            @Override
+            public void call(Object... os) {
+                try {
+                    if (os.length > 0) {
+                        byte[] b = (byte[]) os[0];
+                        writer.writeFile(b);
+                        item.showStatus((int) writer.getPercentage());
+                        table.repaint();
+                        if (!item.isPause()) {
+                            saveFile();
+                        } else {
+                            pause = true;
+                        }
+                    } else {
+                        item.showStatus((int) writer.getPercentage());
+                        item.done();
+                        table.repaint();
+                        writer.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public Object[] toTableRow(int row) {
+        return new Object[]{this, row, fileName, fileSize, "Next Update"};
+    }
+}
